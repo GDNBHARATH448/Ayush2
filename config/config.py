d4b198f73665c2b3a3ed046c1c6ce9ddab5b3c8f
GDNBHARATH448 <gollapallibharath24@gmail.com>
2025-07-25 22:43:30 +0530

    Update config.py
    Signed-off-by: GDNBHARATH448 <gollapallibharath24@gmail.com>

--- config/config.py
+++ config/config.py
@@ -114,10 +114,10 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/TheTeamVivek"
+    "SUPPORT_CHANNEL", "https://t.me/IikiBots"
 )  # Example:- https://t.me/TheTeamVivek
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/TheTeamVk"
+    "SUPPORT_GROUP", "https://t.me/kannada_chatting0"
 )  # Example:- https://t.me/TheTeamVk
 
 
